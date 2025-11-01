8cac9e8ebb25e7774f15d3dabf6be76c65d1ff22
Alex Kam <kamzhanyue@gmail.com>
2025-11-01 18:42:34 +0900

    directional light

---
+++ open-gl/2.lighting/12.1.directional-light/cube-fragment.glsl
@@ -0,0 +1,50 @@
+#version 330 core
+
+struct Material {
+  sampler2D diffuse;
+  sampler2D specular;
+  float shininess;
+};
+
+struct Light {
+  vec3 direction;
+  vec3 ambient;
+  vec3 diffuse;
+  vec3 specular;
+};
+
+in vec3 FragPos;
+in vec3 Normal;
+in vec2 TexCoords;
+
+out vec4 FragColor;
+
+uniform Material material;
+uniform Light light;
+uniform vec3 viewPos;
+
+void main()
+{
+  // Common calculations
+  vec3 lightDir = normalize(-light.direction);
+  vec3 textureColour = vec3(texture(material.diffuse, TexCoords));
+
+  // Calculate Ambient Light
+  vec3 ambient = textureColour * light.ambient;
+
+  // Calculate Diffuse Light
+  vec3 normal = normalize(Normal);
+  float diff = max(dot(normal, lightDir), 0.0);
+  vec3 diffuse = light.diffuse * diff * textureColour;
+
+  // Calculate Specular Light
+  vec3 specularColour = vec3(texture(material.specular, TexCoords));
+  vec3 viewDir = normalize(viewPos - FragPos);
+  vec3 reflectDir = reflect(-lightDir, normal);
+  float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+  vec3 specular = light.specular * (spec * specularColour);
+
+  // Bring it all together
+  vec3 result = diffuse + ambient + specular;
+  FragColor = vec4(result, 1.0);
+}
