4d9dfd1eeaacb0bf980c8eb6553355375312371f
Alex Kam <kamzhanyue@gmail.com>
2025-11-01 22:19:55 +0900

    spot light soft

---
+++ open-gl/2.lighting/12.4.spot-light-soft/cube-fragment.glsl
@@ -0,0 +1,71 @@
+#version 330 core
+
+struct Material {
+  sampler2D diffuse;
+  sampler2D specular;
+  float shininess;
+};
+
+struct FlashLight {
+  vec3 position;
+  vec3 direction;
+  float inner;
+  float outer;
+  vec3 ambient;
+  vec3 diffuse;
+  vec3 specular;
+
+  float constant;
+  float linear;
+  float quadratic;
+};
+
+in vec3 FragPos;
+in vec3 Normal;
+in vec2 TexCoords;
+
+out vec4 FragColor;
+
+uniform Material material;
+uniform FlashLight light;
+uniform vec3 viewPos;
+
+void main()
+{
+  // Common calculations
+  vec3 lightDir = normalize(light.position - FragPos);
+  vec3 textureColour = vec3(texture(material.diffuse, TexCoords));
+
+  // Calculate Ambient Light
+  vec3 ambient = textureColour * light.ambient;
+
+  // Calculate Diffuse Light
+  vec3 normal = normalize(Normal);
+  float diff = max(dot(normal, lightDir), 0.0);
+  vec3 diffuse = light.diffuse * diff * textureColour;
+
+  // Calculate Specular Light
+  vec3 specularColour = vec3(texture(material.specular, TexCoords));
+  vec3 viewDir = normalize(viewPos - FragPos);
+  vec3 reflectDir = reflect(-lightDir, normal);
+  float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+  vec3 specular = light.specular * (spec * specularColour);
+
+  // Spotlight
+  float theta = dot(lightDir, normalize(-light.direction));
+  float epsilon = light.inner - light.outer;
+  float intensity = clamp((theta - light.outer) / epsilon, 0.0, 1.0);
+  
+  diffuse *= intensity;
+  specular *= intensity;
+
+  // Attenuation
+  float distance = length(light.position - FragPos);
+  float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
+  ambient *= attenuation;
+  diffuse *= attenuation;
+  specular *= attenuation;
+
+  vec3 result = diffuse + ambient + specular;
+  FragColor = vec4(result, 1.0);
+}
